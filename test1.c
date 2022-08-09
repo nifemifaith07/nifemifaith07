5e286a5dcaa0c295faf0991ed53fc77e7b3ac7f7
Amuda Faith <77938126+nifemifaith07@users.noreply.github.com>
2022-08-09 23:55:00 +0100

    Update test1.c

--- test1.c
+++ test1.c
@@ -1,10 +1,10 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed
- * @list: list of arguments
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
@@ -33,9 +33,9 @@ int get_precision(const char *format, int *i, va_list list)
 		}
 		else
 			break;
-	}
+		}
 
-	*i = curr_i - 1;
+		*i = curr_i - 1;
 
-	return (precision);
+		return (precision);
 }
